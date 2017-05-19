09cbb9488fa9f8bba78f114145ed68ef4d6cb7ac
andy51002000 <andy51002000@gmail.com>
2017-05-18 22:31:32 -0700

    [20170519] Andy, first push

---
+++ SpeakBot/SpeakBot/Controllers/MessagesController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SpeakBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                // calculate something for us to return
+                int length = (activity.Text ?? string.Empty).Length;
+                List<string> words = new List<string>();
+                words.Add("Acer Explore Beyond LImits.");
+                words.Add("As I know, Acer is number one in the word.");
+                words.Add("JCS is the smartest person i've ever seen");
+                words.Add("Acer is Ace and r so we are the best in the word");
+                words.Add("wao I love Acer products");
+                Random random = new Random();
+                int idx = random.Next(0, words.Count);
+
+                // return our reply to the user
+                Activity reply = activity.CreateReply();
+                reply.Summary = $"You say {activity.Text} which was {length} characters";
+                reply.Speak = $"{words[idx % words.Count]} And you say {activity.Text}";
+                await connector.Conversations.ReplyToActivityAsync(reply);
+                // return our reply to the user
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ SpeakBot/SpeakBot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpeakBot.WebApiApplication" Language="C#" %>
